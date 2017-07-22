fee17f434eec217181b092c81f35860691a514b4
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-22 12:29:22 +0800

    增加30分钟和60分钟测试

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -20,7 +20,9 @@ class Parking < ApplicationRecord
 
   def calculate_amount
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
-      self.amount = 9487
+      if duration <= 60
+        self.amount = 200
+      end
     end
   end
 end
