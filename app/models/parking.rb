860f785ef50816b49cb332915e99a2963cd87298
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-23 09:40:13 +0800

    zengjia 90,61,120分钟的测试“

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -19,9 +19,22 @@ class Parking < ApplicationRecord
   end
 
   def calculate_amount
+    # if self.amount.blank? && self.start_at.present? && self.end_at.present?
+    #   total = 0
+    #   if duration <= 60
+    #     total = 200
+    #   else
+    #     total += 200
+    #     left_duration = duration - 60
+    #     total += (left_duration.to_f / 30).ceil * 100
+    #   end
+    #
+    #   self.amount = total
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
       if duration <= 60
         self.amount = 200
+      else
+        self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
       end
     end
   end
--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -31,5 +31,26 @@ RSpec.describe Parking, type: :model do
       parking.calculate_amount
       expect(parking.amount).to eq(200)
     end
+
+    it "61 mins should be ¥3" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest", :start_at => t, :end_at => t + 61.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(300)
+    end
+
+    it "90 mins should be ¥3" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest", :start_at => t, :end_at => t + 90.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(300)
+    end
+
+    it "120 mins should be ¥4" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest", :start_at => t, :end_at => t + 120.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(400)
+    end
   end
 end
